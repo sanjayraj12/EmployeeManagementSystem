dec16d4ab6783ed3ae89512bc7acc53609dd980c
sanjayraj12 <jsanjayraj012@gmail.com>
2025-10-06 19:27:46 +0530

    Created Backend Part

---
+++ employee-management-system/src/main/java/com/example/ew/controller/EmployeeController.java
@@ -0,0 +1,67 @@
+package com.example.ew.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ew.entity.Employee;
+
+import com.example.ew.serviceimpl.EmployeeServiceImpl;
+
+@RestController
+@RequestMapping("/emp")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeServiceImpl employeeserviceimpl;
+	
+	public EmployeeController( EmployeeServiceImpl employeeserviceimpl) {
+		super();
+		this.employeeserviceimpl = employeeserviceimpl;
+	}
+
+	@PostMapping("/post")
+	public ResponseEntity<Employee>saveemployee( @RequestBody Employee employee)
+	{
+		return new ResponseEntity<Employee>(employeeserviceimpl.saveemployee(employee),HttpStatus.OK);
+		
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<Employee>getempid(@PathVariable long id)
+	{
+		return new ResponseEntity<Employee>(employeeserviceimpl.getempid(id),HttpStatus.OK);
+	}
+	
+	@GetMapping("/all")
+	public List<Employee>getemall(Employee employee)
+	{
+		return employeeserviceimpl.getemall(employee);
+	}
+	
+	@PutMapping("{id}")
+	public ResponseEntity<Employee>updateemp(@PathVariable long id,@RequestBody Employee employee)
+	{
+		return new ResponseEntity<Employee>(employeeserviceimpl.updateemp(employee, id),HttpStatus.OK);
+		
+	}
+	
+	@DeleteMapping("{id}")
+	public String delemp(@PathVariable long id)
+	{
+		employeeserviceimpl.delemp(id);
+		return "Succesfully deleted";
+		
+	}
+
+}
